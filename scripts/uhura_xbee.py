e4bee1e7f3d5df99cea88ceb8e4b77b5879e7ccc
patonz <patonz91@gmail.com>
2021-12-22 12:42:32 +0400

    launch file configuration, topics and node names changes

--- scripts/uhura_xbee.py
+++ scripts/uhura_xbee.py
@@ -43,11 +43,11 @@ DEVICE_NAME = "bee_n"
 device = None
 
 current_message_id = 0
-uav_name = None
-run_type = None
-generic_msg_rcv_pub = None
-position_msg_rcv_pub = None
-list_names=["uav1","uav2","uav3"]
+generic_msg_rcv_pub = rospy.Publisher(
+    'uhuranode/message_received', String, queue_size=10)
+position_msg_rcv_pub = rospy.Publisher(
+    'uhuranodeposition_message_received', Position, queue_size=10)
+
 
 def sendBroadCastData(data):
     #print(sys.getsizeof(data))
@@ -81,21 +81,12 @@ def sendBroadCastData(data):
 
 
 def handle_send_string_data(req):
+    #print("Returning [%s : %s]" % (req.type, req.data))
+   
     print(req.data)
     dataByteArray = bytearray(ToolManager().bitstring_to_bytes(req.data))
     print(dataByteArray)
     print(len(dataByteArray))
-     ########################################################### CHECK IF SIMULATION
-    # TODO FIX IT
-    global run_type, uav_name
-    if run_type == "simulation":
-        for uav in list_names:
-            if uav == uav_name:
-                continue
-            tmp = rospy.Publisher('/%s%s/message_received' % (uav,'/uhuranode'), String, queue_size=10)
-            tmp.publish(req.data)
-        return SendStringDataResponse(True)
-    ############################################################
     return SendStringDataResponse(sendBroadCastData(dataByteArray))
 
 
@@ -107,12 +98,8 @@ def handle_send_position_data(req):
 
 
 def setup(req): #todo false return on exce
-    ########################################################### CHECK IF SIMULATION
-    global run_type
-    if run_type == "simulation":
-        return SetupNetworkDeviceResponse(True)
-    ############################################################
     print("setup: %s" % req)
+
     if req.baudrate is not None:
         baudrate = req.baudrate
     else:
@@ -261,14 +248,13 @@ def start_receiving_data():
         def data_receive_callback(xbee_message):
             rssi = 0
             packet_dict = xbee_message.to_dict()
-            print("RICEVUTOOOOOOOOOOOOOOOOOOOOOOOOOO")
             print(packet_dict)
             print(len(xbee_message.data))
             print("rcv %s %s" % (rssi, ToolManager().bytes_to_bitstring(xbee_message.data)))
             #log_to_file("rcv %s %s" % (rssi, xbee_message.data.decode(errors='ignore').rstrip('\x00')))
             generic_msg_rcv_pub.publish(ToolManager().bytes_to_bitstring(xbee_message.data))
             message_array = parse_message(xbee_message.data.decode(errors='ignore'))
-            print("------------------------------------------------------")
+
             type_message = message_array[0]
             handle_type_message(type_message, message_array)
 
@@ -307,22 +293,23 @@ def log_to_file(data):
 
     return
 def uhura_server():
+
     # rospy.resolve_name(name)
-    global uav_name, run_type
     rospy.init_node('uhuranode', anonymous=False)  # turn off anonymous
+
     node_name = rospy.get_name()
-    print(node_name)
-    uav_name = rospy.get_param("~UAV_NAME")
-    run_type = rospy.get_param("~RUN_TYPE")
     print('node_name: %s ' % (node_name))
-    rospy.Service('%s/send_string_data' % (node_name),SendStringData, handle_send_string_data)
-    rospy.Service('%s/send_position_data' % (node_name), SendPositionData, handle_send_position_data)
-    rospy.Service('%s/setup_network_device' % (node_name), SetupNetworkDevice, setup)
-    rospy.Service('%s/test_broadcast_network' % (node_name), TestBroadcastNetwork, handle_network_test)
-    global generic_msg_rcv_pub, position_msg_rcv_pub
-    generic_msg_rcv_pub = rospy.Publisher('%s/message_received' % (node_name), String, queue_size=10)
-    position_msg_rcv_pub = rospy.Publisher('%s/position_message_received' % (node_name), Position, queue_size=10)
+    rospy.Service('%s/send_string_data' % node_name,
+                  SendStringData, handle_send_string_data)
+    rospy.Service('%s/send_position_data' % node_name, SendPositionData,
+                  handle_send_position_data)
+    rospy.Service('%s/setup_network_device' %
+                  node_name, SetupNetworkDevice, setup)
+    rospy.Service('%s/test_broadcast_network' % node_name,
+                  TestBroadcastNetwork, handle_network_test)
+
     print("Uhura started")
+
     tool_manager_instance = ToolManager()
     print(tool_manager_instance.serial_ports())
     rospy.spin()
